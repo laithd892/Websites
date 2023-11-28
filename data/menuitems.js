c1398ac84304abd52fe162b03a2202b10290052f
LaithDE <laithde@uw.edu>
2023-11-27 20:34:04 -0800

    in the process of adding menu items, not done yet

---
+++ data/menuitems.js
@@ -0,0 +1,21 @@
+const menu = [{
+    name: 'Gyro',
+    descriptions: 'very good food, it will make you happy... hopefully!',
+    priceCents: 1099
+  },
+  {
+    name: 'More Gyros',
+    descriptions: 'Why is this on the menu',
+    priceCents: 1299
+  }, {
+    name: Gyro,
+    descriptions: 'very good food, Don&#8211;t eat it tho',
+    priceCents: 1399
+}];
+
+menu.forEach((menuItem) => {
+  const html = `
+    <button class="button-style">
+      Add Item
+    </button>`
+});
