f7b0782229fb2b061b0c20c7f8262134fbe807e0
LaithDE <laithde@uw.edu>
2023-11-29 14:05:29 -0800

    Tried again to make reservation system

--- Scripts/reservations.js
+++ Scripts/reservations.js
@@ -1,3 +1,12 @@
- function reservationMade() {
-  
- }
+reservationMade();
+//will use this to store reservation date^
+
+//for running reservation date when reservation is made
+
+
+/** 
+function reservationMade() {
+  document.querySelector('.')
+    .innerHTML = `Your reservation on ${} for ${} has been set!`;
+}
+*/
